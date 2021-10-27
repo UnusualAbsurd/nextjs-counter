1031980d175b31479937c9b6073d2f5ab010bc24
UnusualAbsurd <nottherealabsurdchad@gmail.com>
2021-10-27 09:31:55 +0700

    Added warning system to application commands + Warnings Command to application commands

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -43,7 +43,7 @@ module.exports = {
             user.send({
                 embeds: [new MessageEmbed().setAuthor(`${client.user.tag}`, client.user.displayAvatarURL()).setColor(client.config.color).setDescription("You have been warned in " + `**${message.guild.name}**`).setTimestamp().setFooter(`${data._id}-${message.guildId}`)],
                 components: [new MessageActionRow().addComponents([new MessageButton().setLabel("View Reason").setStyle("SECONDARY").setCustomId("viewreason")])]
-            })
+            }).catch(() => {})
         })
 
         await modLog(message, new MessageEmbed()
