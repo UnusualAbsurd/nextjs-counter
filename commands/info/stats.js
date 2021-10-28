3a66e8c03bd8acb6857ed70e4c9d26d540d25ff2
UnusualAbsurd <nottherealabsurdchad@gmail.com>
2021-10-28 11:38:09 +0700

    Bot Statistics COmmand

--- commands/info/stats.js
+++ commands/info/stats.js
@@ -1,15 +1,33 @@
 const { Client, Message, MessageEmbed } = require('discord.js');
+const os = require('os')
+const { commas } = require('number-prettier')
+const ms = require('ms');
 
 module.exports = {
-    name: '',
-    description: '',
-    category: '',
+    name: 'stats',
+    description: 'View statistics of the bot!',
     /** 
      * @param {Client} client 
      * @param {Message} message 
      * @param {String[]} args 
      */
     async execute(client, message, args) {
+
+        message.channel.send({
+            embeds: [
+                new MessageEmbed()
+                .setColor(client.config.color)
+                .setTimestamp()
+                .setAuthor(client.user.username, client.user.displayAvatarURL())
+                .addField(`Servers`, `${commas(client.guilds.cache.size)}`, true)
+                .addField(`Users`, `${commas(client.users.cache.size)}`, true)
+                .addField(`Load Average`, `${os.loadavg().join(", ")}`, true)
+                .addField(`Uptime`, `${ms(client.uptime, { long: true })}`, true)
+                .setFooter(`Requested By: ${message.author.tag}`, message.author.displayAvatarURL({dynamic: true}))
+            ]
+        })
+
+
 
     }
 }   
